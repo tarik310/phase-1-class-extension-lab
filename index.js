8af30143ae3b9569ccec5d6f2358a47f440fb083
tareq <tareq310harh@gmail.com>
2023-04-08 11:50:41 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+// Define the Polygon class
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((acc, cur) => acc + cur, 0);
+    }
+  }
+  
+  // Define the Triangle class
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) {
+        return false;
+      }
+      const [a, b, c] = this.sides;
+      return a + b > c && b + c > a && c + a > b;
+    }
+  }
+  
+  // Define the Square class
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) {
+        return false;
+      }
+      const [a, b, c, d] = this.sides;
+      return a === b && b === c && c === d;
+    }
+  
+    get area() {
+      if (!this.isValid) {
+        return undefined;
+      }
+      const [a, b, c, d] = this.sides;
+      return a * b;
+    }
+  }
+  
